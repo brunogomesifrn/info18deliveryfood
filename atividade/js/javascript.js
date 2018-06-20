044d8ab403951c29ade91f7055e81dcfc5c3365e
karol09 <karolinasilva2000@live.com>
2018-06-20 11:17:12 -0300

    Página Js

---
+++ atividade/js/javascript.js
@@ -0,0 +1,6 @@
+function inserir()
+{document.getElementById('divTeste').innerHTML = '<input id="ce" class="form-control" placeholder="CEP" type="cep"/>';}
+
+
+function apg()
+{document.getElementById('divTeste').innerHTML = '';}
