de3e69a21a6363b92cfa8e2828cc220e6d403ef6
Aline da Silva Bispo <aline.bispo@academico.ifrn.edu.br>
2018-10-31 16:23:29 -0300

    enviando versão mais antiga

---
+++ resources/views/inicio.php
@@ -0,0 +1,170 @@
+
+	@extends('layout.base')
+	
+	@section('conteudo')
+	<div class="row p1" style="padding-top: 50px">
+	<div class="col-md-12">
+	
+		<h2 style="text-align: center; font-family: Arial; color: #1C1C1C" class="p3">Sobre o site</h2>
+	
+	</div>
+	</div>
+	
+	<div class="row">
+	<div class="col-md-3">
+	
+	<img src="images/header.jpg" class="img-circle img-responsive" alt="log do site" width="" height="">
+	
+	</div>
+	
+	<div class="col-md-9 paragrafos">
+	 <p>  texto texto texto texto 
+	 texto texto texto texto texto texto 
+	 texto texto  texto texto  texto texto  
+	 texto texto  texto texto texto texto texto texto texto texto texto 
+	 texto texto texto texto texto texto 
+	 texto texto  texto texto  texto texto  
+	 texto texto  texto texto texto texto texto 
+	 </p>
+
+	
+	</div>
+	
+	
+	</div>
+	
+		<div class="row text-center fh5co-heading">
+		<div class="col-md-12">
+	
+		<h2 style="padding-top: 10px; font-family: Arial; color: #1C1C1C" class="p3 text-center">Visão</h2>
+		</div>
+		 
+		
+	<div class="col-md-9 paragrafos">
+	 <p style="padding-left: 20px"> texto texto texto texto 
+	 texto texto texto texto texto texto 
+	 texto texto  texto texto  texto texto  
+	 texto texto  texto texto texto texto texto 
+	 texto texto texto texto 
+	 texto texto texto texto texto texto 
+	 texto texto  texto texto  texto texto  
+	 texto texto  texto texto texto texto texto 
+	 texto texto texto texto texto texto. 
+	 </p>
+	
+	</div>
+	</div>
+	
+
+	<div class="row text-center fh5co-heading">
+		<div class="col-md-12">
+	
+		<h2 style="padding-top: 10px; font-family: Arial; color: #1C1C1C" class="p3 text-center">Valores</h2>
+		</div>
+		
+	<div class="col-md-9 paragrafos">
+	 <p style="padding-left: 20px">  texto texto texto texto 
+	 texto texto texto texto texto texto 
+	 texto texto  texto texto  texto texto  
+	 texto texto  texto texto texto texto texto 
+	 texto texto texto texto 
+	 texto texto texto texto texto texto 
+	 texto texto  texto texto  texto texto  
+	 texto texto  texto texto texto texto texto 
+	 texto texto texto texto texto texto. 
+	 </p>
+	
+	</div>
+	
+	</div>
+	
+
+
+	
+	<div id="fh5co-menus" data-section="" style="padding-top: 60px; margin-left: 40px">
+			
+			
+				<div class="row text-center fh5co-heading">
+					<div class="col-md-8 col-md-offset-2">
+					<h2 style="font-family: Arial; color: #1C1C1C" class="p3"> Pessoas cadastradas no site</h2>
+					<p style="" class="to-animate p5">Encomende pratos deliciosos diretamente ao cozinheiro ou conzinheira mais
+					próximo da sua cidade.</p>
+					</div>
+				</div>
+				
+				<div class="row row-padded">
+					<div class="col-md-6">
+						<div class="fh5co-food-menu to-animate-2">
+							<ul>
+								<li>
+									<div class="fh5co-food-desc">
+										<figure>
+											<img src="images/header.jpg" class="img-responsive" alt="">
+										</figure>
+										<div>
+											<h3 class="p5">Maria das Dores, doceira e chef de cozinha.</h3>
+											<p class="p5">Ela trabaha na sua residência há 5 anos com encomendas de doces, bolos e salgados para festas também faz entregas em domicílio.
+											Mora em Canguaretana/RN.</p>
+										</div>
+									</div>
+								</li>
+								
+								<li>
+									<div class="fh5co-food-desc">
+										<figure>
+											<img src="images/header.jpg" class="img-responsive" alt="">
+										</figure>
+										<div>
+										<h3 class="p5">Maria das Dores, doceira e chef de cozinha.</h3>
+											<p class="p5">Ela trabaha na sua residência há 5 anos com encomendas de doces, bolos e salgados para festas também faz entregas em domicílio.
+											Mora em Canguaretana/RN.</p>
+										</div>
+									</div>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+					
+					<div class="col-md-6">
+						<div class="fh5co-food-menu to-animate-2">
+							<ul>
+								<li>
+									<div class="fh5co-food-desc">
+										<figure>
+											<img src="images/header.jpg" class="img-responsive" alt="">
+										</figure>
+										<div>
+											<h3 class="p5">Maria das Dores, doceira e chef de cozinha.</h3>
+											<p class="p5">Ela trabaha na sua residência há 5 anos com encomendas de doces, bolos e salgados para festas também faz entregas em domicílio.
+											Mora em Canguaretana/RN.</p>
+										</div>
+									</div>
+								</li>
+								
+								<li>
+									<div class="fh5co-food-desc">
+										<figure>
+											<img src="images/header.jpg" class="img-responsive" alt="">
+										</figure>
+										<div>
+											<h3 class="p5">Maria das Dores, doceira e chef de cozinha.</h3>
+											<p class="p5">Ela trabaha na sua residência há 5 anos com encomendas de doces, bolos e salgados para festas também faz entregas em domicilio.
+											Mora em Canguaretana/RN.</p>
+										</div>
+									</div>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+					
+					</div>
+					
+				</div>
+				
+	</div>
+	
+	
+	@endsection
+	
