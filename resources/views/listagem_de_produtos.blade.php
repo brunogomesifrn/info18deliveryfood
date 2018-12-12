f119b055f551b3f43538faa96f797de975269e9c
Ana Karolina dos Santos Silva <ana.karolina@academico.ifrn.edu.br>
2018-12-12 16:08:55 -0300

    listagem de produtos

--- resources/views/listagem_de_produtos.blade.php
+++ resources/views/listagem_de_produtos.blade.php
@@ -11,6 +11,9 @@
 
 @foreach($produtos as $p)
 	<p> {{$p->nome}} | <a href="/editar_produtos/{{$p->id}}">EDITAR</a> | <a href="remover_produtos/{{$p->id}}">REMOVER</a></p>
+	<p> {{$p->descricao}} | <a href="/editar_produtos/{{$p->id}}">EDITAR</a> | <a href="remover_produtos/{{$p->id}}">REMOVER</a></p>
+	<p> {{$p->valor}} | <a href="/editar_produtos/{{$p->id}}">EDITAR</a> | <a href="remover_produtos/{{$p->id}}">REMOVER</a></p>
+	<p> {{$p->categoria_id_produto}} | <a href="/editar_produtos/{{$p->id}}">EDITAR</a> | <a href="remover_produtos/{{$p->id}}">REMOVER</a></p>
 
 	
 @endforeach
